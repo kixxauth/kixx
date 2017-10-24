ef6797d0d9b57ee811ea9e6710afd7b84a387aea
Kris Walker <kris@kixx.name>
2017-10-24 09:13:12 -0400

    Linted reduction.
     Changes to be committed:
    	modified:   index.js
    	renamed:    lib/types/app.js -> lib/classes/app.js
    	renamed:    lib/types/user-error.js -> lib/classes/user-error.js
    	deleted:    lib/controllers/handle-error.js
    	deleted:    lib/controllers/handle-not-found.js
    	deleted:    lib/controllers/json-api-resource-controller.js
    	deleted:    lib/controllers/resource-controller.js
    	new file:   lib/initialize-app.js
    	renamed:    lib/index.js -> lib/library.js
    	deleted:    lib/namespaces/app.js
    	deleted:    lib/namespaces/json-api.js
    	deleted:    lib/namespaces/router.js
    	deleted:    lib/routes.js
    	renamed:    lib/namespaces/server.js -> lib/start-http-server.js
    	deleted:    lib/types/logger.js
    	deleted:    lib/types/request.js
    	deleted:    lib/types/response.js
    	deleted:    lib/types/router.js
    	modified:   package-lock.json
    	modified:   package.json

--- index.js
+++ index.js
@@ -1,62 +1,6 @@
 'use strict';
 
-const R = require(`ramda`);
-const Filepath = require(`filepath`);
-const UserError = require(`./lib/types/user-error`);
-const Router = require(`./lib/types/router`);
-const server = require(`./lib/namespaces/server`);
-const app = require(`./lib/namespaces/app`);
-const router = require(`./lib/namespaces/router`);
-const routes = require(`./lib/routes`);
+exports.App = require(`./lib/classes/app`);
+exports.UserError = require(`./lib/classes/user-error`);
 
-const startServer = server.start;
-const initializeApp = app.initialize;
-const createRequestHandler = router.createRequestHandler;
-
-const DEFAULT_PORT = 3000;
-const DEFAULT_HOSTNAME = `localhost`;
-
-const defaultPort = R.pathOr(DEFAULT_PORT, [`server`, `port`]);
-const defaultHostname = R.pathOr(DEFAULT_HOSTNAME, [`server`, `hostname`]);
-
-exports.start = function start(dir, env) {
-	const appdir = Filepath.create(dir);
-	const packageJson = appdir.append(`package.json`);
-	const configJs = appdir.append(`config.js`);
-
-	let pkg;
-	try {
-		pkg = require(packageJson.path);
-	} catch (err) {
-		if (err.code === `MODULE_NOT_FOUND`) {
-			throw new UserError(
-				`Missing package.json in the root of your project.`
-			);
-		}
-		throw err;
-	}
-
-	let config;
-	try {
-		config = require(configJs.path);
-	} catch (err) {
-		if (err.code === `MODULE_NOT_FOUND`) {
-			config = {};
-		} else {
-			throw err;
-		}
-	}
-
-	return initializeApp(appdir, pkg, config, env).then((app) => {
-		const port = defaultPort(app.config);
-		const hostname = defaultHostname(app.config);
-
-		const router = routes(app, new Router());
-		const handler = createRequestHandler(router);
-
-		return startServer(handler, hostname, port).then((server) => {
-			return {app, server};
-		});
-	});
-};
-
+exports.initializeApp = require(`./lib/initialize-app`);
